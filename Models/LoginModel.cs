0eb35102f67c4c7c330c87efee5868e7c49178c6
Ziad Arafa <114932522+ZiadArafaa@users.noreply.github.com>
2022-10-03 19:37:36 +0200

    Delete Models directory

--- Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Auth_using_jwt.Models
-{
-    public class LoginModel
-    {
-        public string Email { get; set; }
-        public string PassWord { get; set; }
-    }
-}
--- Models/UserRoleModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Auth_using_jwt.Models
-{
-    public class UserRoleModel
-    {
-        public string UserId { get; set; }
-        public string RoleName { get; set; }
-    }
-}
